72e368a74c43810c66834799e13af7e35d91d14c
clackgot <clackgot@gmail.com>
2022-11-06 02:07:05 +0300

    Добавлены варианты загрузки файла

--- bot.py
+++ bot.py
@@ -17,6 +17,8 @@ dp = Dispatcher(bot)
 async def download_video(message: types.Message):
     print(f'Пришло видео от пользователя {message.from_user}')
     file_id = message.video_note.file_id
+    # file = await bot.get_file(file_id) # Получить файл
+    # await bot.download_file(file.file_path, f'{file_id}.mp4') # Загрузка файл
     await bot.send_video_note(message.from_id, file_id)
 
 if __name__ == '__main__':
